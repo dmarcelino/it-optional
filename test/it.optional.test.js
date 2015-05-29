d6979af9c1ec8f1e0cdbaa015247e2507dbae5d4
Dario Marcelino <dario@appscot.com>
2015-05-29 20:31:51 +0100

    Improve tests

--- test/it.optional.test.js
+++ test/it.optional.test.js
@@ -1,23 +1,42 @@
 it.optional = require('../it.optional');
+var assert = require('assert');
 
 describe('optional tests', function(){
   
-  it.optional('should do xyz', function(done){
-    done();
+  describe('async', function(){
+  
+    it.optional('should do xyz', function(done){
+      done();
+    });
+    
+    it.optional('should do xyz eventually', function(done){
+      done(new Error('oops'));
+    });
+    
+    it.optional('should do ikj eventually', function(done){
+      throw new Error('uncaught error');
+    });
+    
+    it.optional('should do abc eventually', function(done){
+      setTimeout(function(){
+        assert.equal('a', 'b');
+      }, 100);
+    });
+  
   });
   
-  it.optional('should do xyz eventually', function(done){
-    done(new Error('oops'));
+  describe('sync', function(){
+  
+    it.optional('should do xyz', function(){});
+    
+    it.optional('should do xyz eventually', function(){
+      throw new Error('oops');
+    });
+    
+    it.optional('should do abc eventually', function(){
+      assert.equal(1, 2);
+    });
+  
   });
-  
-  it.optional('should do xyz async eventually', function(done){
-    throw new Error('uncaught error');
-  }); 
-  
-  it.optional('should do xyz synced', function(){});
-  
-  it.optional('should do xyz synced eventually', function(){
-    throw new Error('oops');
-  });  
 
 });
